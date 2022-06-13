1761740f0ce13938525665b954d43fe7f8082cb2
bergil <josalmo70@gmail.com>
2022-06-13 18:02:31 +0200

    Add project files.

---
+++ ClimbingTheLeaderboard/ClimbingTheLeaderboard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimbingTheLeaderboard
+{
+    public class ClimbingTheLeaderboard
+    {
+
+        public static List<int> climbingLeaderboard(List<int> ranked, List<int> player) // > a < y < a > respectivamente
+        {
+            List<int> iRetVal = new();
+            Dictionary<int, int> iResultDic = new();
+
+            List<int> iRanked = ranked.Distinct().ToList();
+            int iPlayerLenght = player.Count;
+            int iRankedLenght = iRanked.Count;
+            int j;
+
+            for (int i = 0; i < iPlayerLenght; i++) // De < a >
+            {
+                int iScore = player[i];
+                int iResultItem;
+                if(iResultDic.ContainsKey(iScore))
+                {
+                    iResultItem = iResultDic[iScore];
+                }
+                else
+                {
+                    j = 0;
+                    while (j < iRankedLenght && iRanked[j] > iScore) // De > a <
+                    {
+                        j++;
+                    }
+                    iResultItem = j + 1;
+                    iResultDic.Add(iScore, iResultItem);
+                }                
+                iRetVal.Add(iResultItem);
+            }
+
+            return iRetVal;
+        }
+
+    }
+}
---
+++ ClimbingTheLeaderboard/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+List<int> iRanked = new(new[] { 100, 100, 50, 40, 20, 10});
+List<int> iScore = new(new[] { 5, 25, 50, 120});
+
+Console.WriteLine(String.Join(" ; ", ClimbingTheLeaderboard.ClimbingTheLeaderboard.climbingLeaderboard(iRanked, iScore).Select(x => x.ToString())));
